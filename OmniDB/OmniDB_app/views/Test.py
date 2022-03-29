4f2c1822fdcd10521e1cd0a36407f1de1c13d1c8
yichao.byc <yichao.byc@alibaba-inc.com>
2022-03-29 21:41:55 +0800

    build: 更新requirements.txt
    Signed-off-by: yichao.byc <yichao.byc@alibaba-inc.com>

---
+++ OmniDB/OmniDB_app/views/Test.py
@@ -0,0 +1,12 @@
+v_data = {
+    "v_sql_cmd": "update+a++set++df+",
+    "v_sql_save": "update+a++set++df+",
+    "v_db_index": 1,
+    "v_conn_tab_id": "omnidb_main_tablist_tab3_1640942409216",
+    "v_tab_id": "omnidb_main_tablist_tab3_1640942409216_tabs_tab9_1640942409574",
+    "v_tab_db_id": 7,
+    "v_mode": 0,
+    "v_tab_title": "public.arrow",
+}
+if v_data and isinstance(v_data, dict) and 'v_sql_cmd' in v_data:
+    print(v_data['v_sql_cmd'])
