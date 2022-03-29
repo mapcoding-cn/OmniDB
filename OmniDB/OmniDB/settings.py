8757a48e3ff1e7b207e3dbef6e1e78302ea16542
yichao.byc <yichao.byc@alibaba-inc.com>
2022-03-29 23:18:07 +0800

    feature: LANGUAGE_CODE = 'zh-hans' TIME_ZONE = 'Asia/Shanghai'
    Signed-off-by: yichao.byc <yichao.byc@alibaba-inc.com>

--- OmniDB/OmniDB/settings.py
+++ OmniDB/OmniDB/settings.py
@@ -134,8 +134,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
